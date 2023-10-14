9c61243504785d7eae9d7b350a120acebf714378
JokerMSD <igorpn2004@gmail.com>
2023-10-14 17:25:32 -0300

    feat: create src/pages/ErrorPage/index.jsx

---
+++ src/pages/ErrorPage/index.jsx
@@ -0,0 +1,17 @@
+
+
+export const ErrorPage = () => {
+    
+    return (
+        <div>
+            <h1>Error 404</h1>
+            <p title="Page not found">Página não encontrada!</p>
+            <a href="/dashboard">Go to main page</a>
+            <br/>
+            <a href="/">Go to login page</a>
+            <br/>
+            <a href="/register">Go to register page</a>
+            <br/>
+        </div>
+    );
+};
