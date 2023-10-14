6fac0e8395cf04b1661d94d6b9dee7fc602b5343
JokerMSD <igorpn2004@gmail.com>
2023-10-13 22:36:52 -0300

    update src/pages/login/index.jsx

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,9 +1,75 @@
-export function Login () {
+import Style from "./style.module.scss";
+
+export function Login() {
+    
+  return (
+    <>
+      <main>
+
+        <section className={Style.loginSection}>
+
+          <img 
+            className={Style.loginLogo}
+            width={200} 
+            height={200} 
+            src="../src/assets/logo.svg" 
+            alt="Site Logo"/>
+
+            <div className={Style.loginContainer}>
+
+             <h1 className={Style.loginTitle}>Login</h1>
+
+              <form className={Style.loginFormContainer} action="submit">
+
+                <label 
+                  className={Style.label}
+                  htmlFor="email" 
+                >Email</label>
+
+                <input
+                className={Style.inputLogin}
+                  name="email"
+                  id="email"
+                  type="text" 
+                  placeholder="Email" 
+                />
+
+                <h2 
+                  className={Style.label}
+                  htmlFor="password" 
+                >Senha</h2>
+
+                <input 
+                  className={Style.inputLogin}
+                  name="password"
+                  id="password"
+                  type="password" 
+                  placeholder="Senha" 
+                />
+
+                <button 
+                className={Style.loginBtn} 
+                type="submit">Entrar</button>
+
+               </form>
+                <div className={Style.resgisterContainer}>
+               
+                   <small className={Style.small}>Ainda não possui uma conta?</small>
+
+                   <button 
+                   className={Style.registerBtn}
+                   onClick={() => window.location.replace("../register")}  
+                   >Cadastre-se</button>
+               
+                </div>
 
 
-    return (
-      <>
-      
-      </>
-    )
+
+            </div>
+
+        </section>
+
+      </main>
+    </>
+  );
 }
