b82e5cd26140323e168d55bcb19d9f00d45ac462
JokerMSD <igorpn2004@gmail.com>
2023-10-15 00:14:33 -0300

    update src/pages/register/index.jsx

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,11 +1,44 @@
+import { RegisterForm } from "../../components/forms/RegisterForm";
+import Style from "./style.module.scss";
 
+export function Register() {
+  return (
+    <>
+      <main>
+        <section className={Style.registerSection}>
 
-export function Register () {
+          <div className={Style.registerBackHeader}>
 
+          <img
+            className={Style.registerLogo}
+            width={200}
+            height={200}
+            src="../src/assets/logo.svg"
+            alt="Site Logo"
+          />
+          <button 
+          className={Style.backButton}
+          onClick={() => window.location.replace("/")} 
+          type="button" 
+          title="Voltar" 
+          aria-label="Voltar" 
+          >Voltar</button>
 
-    return (
-      <>
-      <h2>olá</h2>
-      </>
-    )
+          </div>
+
+          <div className={Style.registerContainer}>
+
+            <h1 className={Style.registerTitle}>Crie sua conta</h1>
+
+            <small className={Style.registerSubtitle}>
+              Rapido e grátis, vamos nessa
+            </small>
+
+            <RegisterForm />
+          </div>
+          
+        </section>
+      </main>
+    </>
+  );
 }
