937446458b1919e7939d8890f2c50ddee3c32060
JokerMSD <igorpn2004@gmail.com>
2023-10-16 17:06:56 -0300

    update src/pages/RegisterPage/index.jsx

--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -3,33 +3,30 @@ import { RegisterForm } from "../../components/forms/RegisterForm";
 import Style from "./style.module.scss";
 
 export function Register() {
-  
   return (
     <>
       <main>
         <section className={Style.registerSection}>
-
           <div className={Style.registerBackHeader}>
-
-          <img
-            className={Style.registerLogo}
-            width={200}
-            height={200}
-            src="../src/assets/logo.svg"
-            alt="Site Logo"
-          />
-          <Link 
-          className={Style.backButton}
-          to="/"
-          type="button" 
-          title="Voltar" 
-          aria-label="Voltar" 
-          >Voltar</Link>
-
+            <img
+              className={Style.registerLogo}
+              width={200}
+              height={200}
+              src="../src/assets/logo.svg"
+              alt="Site Logo"
+            />
+            <Link
+              className={Style.backButton}
+              to="/"
+              type="button"
+              title="Voltar"
+              aria-label="Voltar"
+            >
+              Voltar
+            </Link>
           </div>
 
           <div className={Style.registerContainer}>
-
             <h1 className={Style.registerTitle}>Crie sua conta</h1>
 
             <small className={Style.registerSubtitle}>
@@ -38,7 +35,6 @@ export function Register() {
 
             <RegisterForm />
           </div>
-          
         </section>
       </main>
     </>
