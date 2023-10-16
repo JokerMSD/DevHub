4863d6647609a84c188e2fdb9486b0a9d15e478d
JokerMSD <igorpn2004@gmail.com>
2023-10-16 05:19:30 -0300

    update src/components/Input/index.jsx

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,11 +1,12 @@
 import { forwardRef } from "react";
 import Style from "./style.module.scss";
 
-export const Input = forwardRef(({ label, id, ...rest }, ref) => {
+export const Input = forwardRef(({ error, label, id, ...rest }, ref) => {
     return (
-        <div>
+        <div className={Style.inputContainer}>
             <label className={Style.label} htmlFor={id}>{label}</label>
             <input ref={ref} {...rest} />
+            {error ? <p className={Style.error}>{error.message}</p> : null}
         </div>
     );
 });
