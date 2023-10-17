436fd1144158cba2cca756d1a0c9f9add1d8be79
JokerMSD <igorpn2004@gmail.com>
2023-10-17 19:30:16 -0300

    update src/pages/ErrorPage/index.jsx

--- src/pages/ErrorPage/index.jsx
+++ src/pages/ErrorPage/index.jsx
@@ -1,17 +1,14 @@
-
+import { Link } from "react-router-dom";
+import Style from "./style.module.scss"
+import errorLogo from "../../assets/pngegg.png"
 
 export const ErrorPage = () => {
     
     return (
-        <div>
-            <h1>Error 404</h1>
-            <p title="Page not found">Página não encontrada!</p>
-            <a href="/dashboard">Go to main page</a>
-            <br/>
-            <a href="/">Go to login page</a>
-            <br/>
-            <a href="/register">Go to register page</a>
-            <br/>
+        <div className={Style.ErrorPage}>
+            <img src={errorLogo} className={Style.errorlogo}/>
+            <p className={Style.errorSub} title="Page not found">Página não encontrada!</p>
+            <Link to={"/"} className={Style.back}>Voltar</Link>
         </div>
     );
 };
