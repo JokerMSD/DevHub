f0963d4c6533791ace0360224c07db2dec349e70
JokerMSD <igorpn2004@gmail.com>
2023-10-17 18:59:52 -0300

    update src/routes/index.jsx

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -3,13 +3,15 @@ import { Login } from "../pages/HomePage";
 import { Register } from "../pages/RegisterPage";
 import { Dashboard } from "../pages/DashboardPage";
 import { ErrorPage } from "../pages/ErrorPage";
+import { useState } from "react";
 
 export const Router = () => {
+  const [user, setUser] = useState([])
   return ( 
     <Routes>
-        <Route path="/" element={<Login/>}/>
+        <Route path="/" element={<Login user={user} setUser={setUser}/>}/>
         <Route path="/register" element={<Register/>}/>
-        <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/dashboard" element={<Dashboard user={user} setUser={setUser}/>}/>
         <Route path="*" element={<ErrorPage/>}/>
     </Routes>
 
