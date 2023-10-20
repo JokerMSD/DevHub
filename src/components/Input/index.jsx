1696005a92a739e709d28efcd7faf49f83f9e34d
JokerMSD <igorpn2004@gmail.com>
2023-10-20 16:01:44 -0300

    update src/components/Input/index.jsx

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -6,51 +6,49 @@ import { BsEyeFill, BsEyeSlashFill } from "react-icons/bs";
 
 export const Input = forwardRef(
   ({ error, noHaveSlash, noEye, type, label, id, ...rest }, ref) => {
-
     const [isPasswordVisible, setPasswordVisible] = useState(false);
 
     const viewPassword = () => {
-        setPasswordVisible(true);
-        const password = document.querySelector(`input[name=${id}]`);
-        if(password.type === "password"){
-            password.type = "text";
-        }
-      };
-      
-      const hidePassword = () => {
-        setPasswordVisible(false);
-        const password = document.querySelector(`input[name=${id}]`);
-        if(password.type === "text"){
-            password.type = "password";
-        }
-      };
-      
+      setPasswordVisible(true);
+      const password = document.querySelector(`input[name=${id}]`);
+      if (password.type === "password") {
+        password.type = "text";
+      }
+    };
+
+    const hidePassword = () => {
+      setPasswordVisible(false);
+      const password = document.querySelector(`input[name=${id}]`);
+      if (password.type === "text") {
+        password.type = "password";
+      }
+    };
 
     return (
       <div className={Style.inputContainer}>
-
         <label className={Style.label} htmlFor={id}>
           {label}
         </label>
 
         <div className={Style.inputDiv}>
+          <input type={type} ref={ref} {...rest} />
 
-        <input type={type} ref={ref} {...rest} />
-
-
-        <BsEyeFill
-          onClick={viewPassword}
-          className={`${Style.eye} ${noEye || isPasswordVisible ? Style.hidden : ""}`}
+          <BsEyeFill
+            onClick={viewPassword}
+            className={`${Style.eye} ${
+              noEye || isPasswordVisible ? Style.hidden : ""
+            }`}
           />
 
-        <BsEyeSlashFill
-        onClick={hidePassword}
-        className={`${Style.eyeSlashed} ${noHaveSlash || !isPasswordVisible ? Style.hidden : ""}`}
-        />
-
+          <BsEyeSlashFill
+            onClick={hidePassword}
+            className={`${Style.eyeSlashed} ${
+              noHaveSlash || !isPasswordVisible ? Style.hidden : ""
+            }`}
+          />
         </div>
         {error ? <p className={Style.error}>{error.message}</p> : null}
       </div>
     );
-  }
+  },
 );
